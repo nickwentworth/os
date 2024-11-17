74e8b515c5a718f8d4743e88e63304bc4bfa3ebf
Nick <nickwentworth123@gmail.com>
2024-11-16 23:41:28 -0500

    adjusted boot.s to halt extra cpus

--- src/boot.s
+++ src/boot.s
@@ -2,9 +2,12 @@
 
 .globl _start
 
-.org 0x80000
-
 _start:
+    // halt all but main processor
+    mrs     x5, MPIDR_EL1
+    and     x5, x5, #0b11
+    cbnz    x5, halt
+
     // initialize stack
     ldr     x5, =_start
     mov     sp, x5
@@ -20,7 +23,8 @@ _start:
 // jump to rust code, which shouldn't return
 2:  bl      _kernel_main
 
-// failsafe loop, shouldn't reach this
+// loop forever, send extra cpus here
+// TODO: figure out what to do with them
 halt:
     wfe
     b halt
