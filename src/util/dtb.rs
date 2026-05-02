8b2a1b45381ea8d643b861b36dea81909a17064d
Nick <nickwentworth123@gmail.com>
2026-05-01 22:18:15 -0400

    improved dtb and node implementation

--- src/util/dtb.rs
+++ src/util/dtb.rs
@@ -1,6 +1,22 @@
-use core::ffi::CStr;
+use crate::mem::addr::KernelVirtAddr;
 
-use crate::{mem::addr::KernelVirtAddr, println};
+// TODO: maybe relocate these helper functions?
+unsafe fn read_u32_be(ptr: *mut u32) -> u32 {
+    u32::from_be(*ptr)
+}
+unsafe fn read_null_terminated_str<'a>(start: *mut u8) -> &'a str {
+    let mut len = 0;
+
+    while *start.add(len) != 0 {
+        len += 1;
+    }
+
+    let slice = core::slice::from_raw_parts(start, len);
+    core::str::from_utf8(slice).unwrap()
+}
+fn align_up(value: usize, align: usize) -> usize {
+    (value + align - 1) & !(align - 1)
+}
 
 #[derive(Debug)]
 pub enum DtbParseErr {
@@ -42,38 +58,33 @@ impl DeviceTree {
             let ptr = self.base_addr.to_ptr().cast::<u32>();
 
             DeviceTreeHeader {
-                magic: Self::read_u32(ptr),
-                total_size: Self::read_u32(ptr.wrapping_add(1)),
-                offset_dt_struct: Self::read_u32(ptr.wrapping_add(2)),
-                offset_dt_strings: Self::read_u32(ptr.wrapping_add(3)),
-                offset_mem_rsvmap: Self::read_u32(ptr.wrapping_add(4)),
-                version: Self::read_u32(ptr.wrapping_add(5)),
-                last_compat_version: Self::read_u32(ptr.wrapping_add(6)),
-                boot_cpu_id: Self::read_u32(ptr.wrapping_add(7)),
-                size_dt_strings: Self::read_u32(ptr.wrapping_add(8)),
-                size_dt_struct: Self::read_u32(ptr.wrapping_add(9)),
+                magic: read_u32_be(ptr),
+                total_size: read_u32_be(ptr.add(1)),
+                offset_dt_struct: read_u32_be(ptr.add(2)),
+                offset_dt_strings: read_u32_be(ptr.add(3)),
+                offset_mem_rsvmap: read_u32_be(ptr.add(4)),
+                version: read_u32_be(ptr.add(5)),
+                last_compat_version: read_u32_be(ptr.add(6)),
+                boot_cpu_id: read_u32_be(ptr.add(7)),
+                size_dt_strings: read_u32_be(ptr.add(8)),
+                size_dt_struct: read_u32_be(ptr.add(9)),
             }
         }
     }
 
-    pub fn nodes(&self) -> DeviceTreeNodeIter {
+    pub fn nodes<'a>(&'a self) -> DtbNodeIter<'a> {
         let header = self.header();
 
         let start = self
             .base_addr
             .to_ptr()
-            .wrapping_byte_add(header.offset_dt_struct as usize);
+            .wrapping_byte_add(header.offset_dt_struct as usize)
+            .cast();
 
-        DeviceTreeNodeIter::from_addr(start)
-    }
-
-    unsafe fn read_u32(ptr: *mut u32) -> u32 {
-        u32::from_be(*ptr)
-    }
-
-    fn align_up(addr: *mut u8, align: usize) -> *mut u8 {
-        let addr_u = addr as usize;
-        ((addr_u + align - 1) & !(align - 1)) as *mut u8
+        DtbNodeIter {
+            dt: self,
+            cursor: start,
+        }
     }
 }
 
@@ -92,72 +103,88 @@ pub struct DeviceTreeHeader {
     size_dt_struct: u32,
 }
 
-/// Helper struct to iterate through the main nodes of the `DeviceTree`
-pub struct DeviceTreeNodeIter {
-    cursor: *mut u8,
+#[derive(Debug)]
+enum DtbToken {
+    BeginNode { name_start_ptr: *mut u8 },
+    EndNode,
+    Prop { value_len: u32, name_offset: u32 },
+    Nop,
+    End,
 }
 
-impl DeviceTreeNodeIter {
-    fn from_addr(start: *mut u8) -> Self {
-        Self { cursor: start }
+impl DtbToken {
+    unsafe fn from_ptr(ptr: *mut u32) -> Result<Self, u32> {
+        let token = read_u32_be(ptr);
+
+        match token {
+            0x00000001 => Ok(Self::BeginNode {
+                name_start_ptr: ptr.add(1).cast::<u8>(),
+            }),
+            0x00000002 => Ok(Self::EndNode),
+            0x00000003 => Ok(Self::Prop {
+                value_len: read_u32_be(ptr.add(1)),
+                name_offset: read_u32_be(ptr.add(2)),
+            }),
+            0x00000004 => Ok(Self::Nop),
+            0x00000009 => Ok(Self::End),
+            _ => Err(token),
+        }
+    }
+
+    fn size(&self) -> usize {
+        match self {
+            Self::BeginNode { name_start_ptr } => {
+                let name = unsafe { read_null_terminated_str(*name_start_ptr) };
+                4 + align_up(name.len() + 1, 4)
+            }
+            Self::EndNode => 4,
+            Self::Prop { value_len, .. } => 12 + align_up(*value_len as usize, 4),
+            Self::Nop => 4,
+            Self::End => 4,
+        }
     }
 }
 
-impl Iterator for DeviceTreeNodeIter {
-    type Item = DeviceTreeNode;
+/// Helper struct to iterate through the main nodes of the `DeviceTree`
+pub struct DtbNodeIter<'dtb> {
+    dt: &'dtb DeviceTree,
+    cursor: *mut u32,
+}
+
+impl<'dtb> Iterator for DtbNodeIter<'dtb> {
+    type Item = DtbNode<'dtb>;
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            let curr_token = unsafe { DeviceTree::read_u32(self.cursor.cast()) };
+            // println!("Reading token @ {:x}", self.cursor as usize);
+            let token = unsafe { DtbToken::from_ptr(self.cursor).unwrap() };
+            // println!("Found token: {:?}", token);
 
-            match curr_token {
-                0x00000001 => {
-                    let node = DeviceTreeNode::from_addr(self.cursor);
+            match token {
+                DtbToken::BeginNode { .. } => {
+                    let node = DtbNode {
+                        dt: self.dt,
+                        base_addr: self.cursor.cast(),
+                    };
 
-                    // We found the start of the node, but we still need to advance to the next token
-                    let offset = 4 + node.name().len() + 1; // handle size of current token + name + null char
-                    self.cursor = self.cursor.wrapping_add(offset);
-                    self.cursor = DeviceTree::align_up(self.cursor, 4);
+                    self.cursor = self.cursor.wrapping_byte_add(token.size());
 
                     return Some(node);
                 }
-                0x00000002 => self.cursor = self.cursor.wrapping_add(4),
-                0x00000003 => {
-                    self.cursor = self.cursor.wrapping_add(4); // advance to property info struct
-
-                    let prop_len = unsafe { DeviceTree::read_u32(self.cursor.cast()) };
-
-                    let offset = 8 + prop_len as usize; // handle size of info struct + property value
-                    self.cursor = self.cursor.wrapping_add(offset);
-                    self.cursor = DeviceTree::align_up(self.cursor, 4);
-                }
-                0x00000004 => self.cursor = self.cursor.wrapping_add(4),
-                0x00000009 => return None,
-                _ => return None, // TODO: should this panic?
+                DtbToken::End => return None,
+                _ => self.cursor = self.cursor.wrapping_byte_add(token.size()),
             }
         }
     }
 }
 
-pub struct DeviceTreeNode {
+pub struct DtbNode<'dtb> {
+    dt: &'dtb DeviceTree,
     base_addr: *mut u8,
 }
 
-impl DeviceTreeNode {
-    fn from_addr(addr: *mut u8) -> Self {
-        Self { base_addr: addr }
-    }
-
+impl<'dtb> DtbNode<'dtb> {
     pub fn name(&self) -> &str {
         let name_ptr = self.base_addr.wrapping_add(4);
-        let mut name_len = 0;
-
-        unsafe {
-            while *name_ptr.add(name_len) != 0 {
-                name_len += 1;
-            }
-
-            let slice = core::slice::from_raw_parts(name_ptr, name_len);
-            core::str::from_utf8(slice).unwrap()
-        }
+        unsafe { read_null_terminated_str(name_ptr) }
     }
 }
